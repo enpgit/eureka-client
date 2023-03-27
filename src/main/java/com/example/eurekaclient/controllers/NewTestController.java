a073a29e777c83f76117d0b621baf2a360314fb9
enpas <e.n.pashkin@gmail.com>
2023-03-28 00:48:54 +0300

    Запуск нескольких клиентов, как работает балансировщик загрузки

--- src/main/java/com/example/eurekaclient/controllers/NewTestController.java
+++ src/main/java/com/example/eurekaclient/controllers/NewTestController.java
@@ -1,5 +1,6 @@
 package com.example.eurekaclient.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/test")
 public class NewTestController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String idName;
+
     @GetMapping(value = "/main")
     public String testView() {
-        return "Privet";
+        return "idName: " + idName;
     }
 }
